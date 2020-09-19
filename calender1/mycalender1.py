b85bc958ac0ea367f75676e0348673b1515997af
bignatezzz <aashzzz786@gmail.com>
2020-09-18 18:02:02 -0700

    calander 1 program

---
+++ calender1/mycalender1.py
@@ -0,0 +1,11 @@
+#
+# Program: This is an example program that prints my school schedule.
+#
+# Author: Aashrith
+# Date : 9/18/20
+a = """Day           Time.                        Subject
+Monday   10:00 - 11:00 AM    Period 1
+                  11:30-12:30 AM.    Math
+Tuesday     9:00-11:00 AM     PE
+                  10:00-11:00 AM.    Science"""
+print(a)
