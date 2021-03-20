88739cf7b0783b33f69697da8fa48b96779ef876
bignatezzz <aashzzz786@gmail.com>
2021-03-20 15:14:44 -0700

    Removed pre-defined keys

--- buildschedule1/buildschedule1.py
+++ buildschedule1/buildschedule1.py
@@ -8,14 +8,10 @@ mylines = myfile.readlines()
 
 
 #define a dictionary to gather schedule per day
-thisdict = {
-   "Mon": [],
-   "Tue": [],
-   "Wed": [],
-   "Thu": [],
-   "Fri": []
-}
+thisdict = {}
 
+#define the order of printing a schedule
+days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 
 #print each line from the file
 #process one line at a time
@@ -24,20 +20,22 @@ for myline in mylines:
     myline_list = myline.split(",")
     #print str(myline_list)
     #print myline
+    day = myline_list[0]
     #check if the first element in the array is one of the days in the dictionary
-    if myline_list[0] in thisdict.keys():
-       #print myline_list[0]
+    if day in days:
+       #if the day is not one of the keys create an empty list for the day so you can add data later
+       if day not in thisdict.keys():
+          thisdict[day] = []
+       #print day
        #getting information after the first element or day
        sched=myline_list[1:]
        #print str(sched)
        #we are adding the schedule from the line to the list
-       thisdict[myline_list[0]].append(sched)
+       thisdict[day].append(sched)
     else:
        #if the day is not valid print error
-       print "Invalid Day "+ str(myline_list[0])
+       print "Invalid Day "+ day
 
-#define the order of printing a schedule
-days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 #pick a day in the defined order
 for day in days:
     #find the schedule for that day in the dictionary
